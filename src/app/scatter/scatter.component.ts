cebcccafff02a827f8a670392a85727d0d8c66c9
dl9133 <dlswe9133@gmail.com>
2025-02-18 01:21:13 -0500

    added angular components

---
+++ src/app/scatter/scatter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scatter',
+  imports: [],
+  templateUrl: './scatter.component.html',
+  styleUrl: './scatter.component.css'
+})
+export class ScatterComponent {
+
+}
